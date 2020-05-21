952ec113c4dba5baf4227286a611e5cf336f6d85
Raoul <raoulschmid@bluewin.ch>
2020-05-21 22:30:42 +0200

    DI

--- app/core/Main.php
+++ app/core/Main.php
@@ -1,14 +1,51 @@
 <?php
 
+namespace nofw\core;
+
 use DI\Container;
+use League\Route\Router;
+use League\Route\Strategy\ApplicationStrategy;
+use nofw\middlewares\AuthMiddleware;
+use nofw\middlewares\CorsMiddleware;
 use nofw\services\RoutingService;
 
 class Main {
+    private $container;
+    private $router;
 
     public function __construct() {
-        $container = new Container();
-        $routing = new RoutingService($container);
+        $this->container = new Container();
+        //$this->router = new Router();
+        //$routingService = $this->container->get(RoutingService::class);
+        //$this->container->set('router', \DI\create(Router::class));
+        //$this->container->set('routingService', \DI\create(RoutingService::class)->constructor($this->router));
+        $this->router = $this->container->get(Router::class);
+
+        $this->setupRouter();
+        $this->setupMiddleware();
+
+        $this->container->get(RoutingService::class);
+
     }
+
+    private function setupRouter() {
+        $strategy = new ApplicationStrategy();
+        $strategy->setContainer($this->container);
+        $this->router->setStrategy($strategy);
+
+        //new RoutingService($this->router);
+        //$this->container->call('routingService');
+        //$this->container->get('routingService');
+
+    }
+
+    private function setupMiddleware() {
+        $this->router->middlewares([
+            new CorsMiddleware(),
+            new AuthMiddleware()
+        ]);
+    }
+
 }
 
 new Main();
--- app/services/RoutingService.php
+++ app/services/RoutingService.php
@@ -13,41 +13,32 @@ use Laminas\Diactoros\ServerRequestFactory;
 use DI\Container;
 use nofw\middlewares\AuthMiddleware;
 use nofw\middlewares\CorsMiddleware;
+use nofw\core\NofwApp;
 
 class RoutingService {
 
-    private $container;
     private $configService;
+    private $router;
 
-    public function __construct(Container $container) {
-        $this->container = $container;
-        $this->configService = $container->get(ConfigService::class);
+    public function __construct(Router $router) {
+        //$this->configService = $configService;
+        $this->router = $router;
         $this->route();
     }
 
     private function route() {
-        $basePath = $this->configService->basePath;
+        $basePath = '/unicate/no-framework/public';//$this->configService->basePath;
 
         $request = ServerRequestFactory::fromGlobals(
             $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
         );
-        $strategy = new ApplicationStrategy();
-        $strategy->setContainer($this->container);
-
-        $router = new Router();
-        $router->setStrategy($strategy);
-        $router->middlewares([
-            new CorsMiddleware(),
-            new AuthMiddleware(),
-        ]);
-
         $routes = include(__DIR__ . '/../config/routes.php');
 
         foreach ($routes as $route) {
-            $router->addRoute($route['method'], $basePath . $route['path'], $route['handler']);
+            $this->router->addRoute($route['method'], $basePath . $route['path'], $route['handler']);
         }
         try {
-            $response = $router->dispatch($request);
+            $response = $this->router->dispatch($request);
         } catch (NotFoundException $exception) {
             $response = (new ResponseFactory())->createResponse()
                 ->withHeader('Location', $basePath . '/sorry')
